88a4fd52b016ce1b9d406a3941131b4e7cc70551
Israiloff <stardustindark@gmail.com>
2024-07-29 00:05:59 +0500

    added: nvim lazy manager update on docker image building feature, ignoring cache on image building; modified: version to 0.0.3;

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,8 @@
-BUILD_VERSION=0.0.2
+BUILD_VERSION=0.0.3
 docker build \
             --build-arg JDK_VERSION=21 \
 			--build-arg PYTHON_VERSION=3 \
             --build-arg TIMEZONE=Asia/Tashkent \
+			--no-cache \
             -t israiloff/jvim:$BUILD_VERSION .
 docker tag israiloff/jvim:$BUILD_VERSION israiloff/jvim:latest
